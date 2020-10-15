2032f0e3d2216270760fae00745162a58a578412
Ivan Mincione <ivan.mincione92@outlook.it>
2020-10-15 17:19:16 +0200

    js-jsnack7
    problema con  il calcolo della media

---
+++ js-jsnack7/main.js
@@ -0,0 +1,17 @@
+// Calcola la somma e la media dei numeri da 1 a 10.
+
+var array = [1,2,3,4,5,6,7,8,9,10];
+
+var somma = 0;
+var media = 0;
+
+for(var i=0; i < array.length; i++) {
+    // calcolo la somma dei numeri contenuti nell'array
+    somma = somma + array[i];
+
+    // calcolo la media
+    media = somma / array.lenght;
+}
+
+console.log(somma);
+console.log(media);
