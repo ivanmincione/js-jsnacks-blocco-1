f4ceb0ea7cc81b8ba091f035f630cbf2af2bba66
Ivan Mincione <ivan.mincione92@outlook.it>
2020-10-15 16:07:08 +0200

    js-jsnack5

---
+++ js-jsnack5/main.js
@@ -0,0 +1,15 @@
+// Stampa il cubo dei primi N numeri, dove N è un numero
+// indicato dall’utente.
+
+// chiedo all'utente di inserire un numero
+var number = parseInt(prompt('inserisci un numero'));
+
+// ciclo for con proprietà Math.pow per calcolare il cubo del numero
+
+for (var i = 0; i <= number; i++) {
+// variabile di supporto
+    var cube = Math.pow(i, 3);
+}
+
+// stampo il risultato del cubo
+console.log(cube);
