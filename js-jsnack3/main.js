ad4a7dec6c09cbe1b98e0505845100c7a1ed2e1a
Ivan Mincione <ivan.mincione92@outlook.it>
2020-10-15 15:34:58 +0200

    Modificato js-jsnack3 / inizio js-jsnack4

--- js-jsnack3/main.js
+++ js-jsnack3/main.js
@@ -2,35 +2,15 @@
 // numero. Il programma stampa la somma di tutti i numeri
 // inseriti.
 
+// creo una variabile di supporto
+var somma = 0;
 
-// chiedo per 5 volte all'utente l'inserimento di un numero
-var numero1 = parseInt(prompt("Inserisci un numero qualsiasi"));
-var numero2 = parseInt(prompt("Inserisci un numero qualsiasi"));
-var numero3 = parseInt(prompt("Inserisci un numero qualsiasi"));
-var numero4 = parseInt(prompt("Inserisci un numero qualsiasi"));
-var numero5 = parseInt(prompt("Inserisci un numero qualsiasi"));
-
-// stampo i numeri inseriti
-console.log( numero1 );
-console.log( numero2 );
-console.log( numero3 );
-console.log( numero4 );
-console.log( numero5 );
-
-// creo un array per contenere i numeri inseriti dall'utente
-var list = [];
-// aggiungo all'array i numeri inseriti dall'utente
-list.push (numero1);
-list.push (numero2);
-list.push (numero3);
-list.push (numero4);
-list.push (numero5);
-
-console.log(list);
-
-var somma = 0
-
-for (var i = 0; i < list.length; i++) {
-    somma = somma + list[i]
-    console.log("somma" + somma);
+// inserisco nel ciclo for l'input dei numeri scelti dall'utente e la somma tra essi
+for (var i = 0; i < 5; i++){
+  var numero = parseInt(prompt('Inserisci un numero'));
+  console.log(numero);
+  somma = somma + numero;
 }
+
+// stampo la somma
+console.log(somma);
---
+++ js-jsnack4/main.js
@@ -0,0 +1,20 @@
+// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire
+// un numero, se è dispari inseriscilo nell’array.
+
+// creazione di un array vuoto
+
+var array = [];
+
+// chiedere all'utente di inserire 6 numeri
+
+var numero1 = parseInt(prompt("Inserisci un numero qualsiasi"));
+var numero2 = parseInt(prompt("Inserisci un numero qualsiasi"));
+var numero3 = parseInt(prompt("Inserisci un numero qualsiasi"));
+var numero4 = parseInt(prompt("Inserisci un numero qualsiasi"));
+var numero5 = parseInt(prompt("Inserisci un numero qualsiasi"));
+
+// se il numer oinserito è dispari inserisci quest'ultimo nell'array
+
+for (var i = 0; i < 6; i++) {
+    
+}
